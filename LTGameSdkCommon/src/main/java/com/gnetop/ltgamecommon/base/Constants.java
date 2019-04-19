5096b22619d5cb4a5d6f0b9f0c72b0d41dcd1570
silence <yangkemuyi@sina.com>
2019-04-19 11:03:26 +0800

    更新乐推apiToken

---
+++ LTGameSdkCommon/src/main/java/com/gnetop/ltgamecommon/base/Constants.java
@@ -0,0 +1,13 @@
+package com.gnetop.ltgamecommon.base;
+
+public class Constants {
+    /***用户协议标志*/
+    public static final String USER_AGREEMENT_FLAT = "USER_AGREEMENT_FLAT";
+    /***乐推UID标志*/
+    public static final String USER_LT_UID = "USER_FACEBOOK_LT_UID";
+    /***乐推UIDtoken标志*/
+    public static final String USER_LT_UID_TOKEN = "USER_FACEBOOK_LT_UID_TOKEN";
+    /***apiToken标志*/
+    public static final String USER_API_TOKEN = "USER_API_TOKEN";
+
+}
